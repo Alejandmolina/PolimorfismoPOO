4e3a1b24076cc976f728c521685f4c0cdcac6cbe
Alejandro Molina <playrenealejand@gmail.com>
2023-03-13 22:42:40 -0600

    Ejercicio2 Herencia y Polimorfismo - Automovil

---
+++ Metodos.py
@@ -0,0 +1,11 @@
+from Automovil import Automovil
+from Motocicleta import Motocicleta
+from Bicicleta import Bicicleta
+
+automovil = Automovil()
+motocicleta = Motocicleta()
+bicicleta = Bicicleta()
+
+automovil.moverse()    
+motocicleta.moverse()  
+bicicleta.moverse()    
