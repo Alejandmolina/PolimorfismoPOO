741728d63c116d043ced4ebeb0702d3c297f4a1b
Alejandro Molina <playrenealejand@gmail.com>
2023-03-13 22:42:03 -0600

    Ejercicio2 Herencia y Polimorfismo - Automovil

---
+++ Vehiculo.py
@@ -0,0 +1,6 @@
+class Vehiculo:
+    def __init__(self, velocidad_maxima):
+        self.velocidad_maxima = velocidad_maxima
+
+    def moverse(self):
+        print("El vehículo se está moviendo...")
